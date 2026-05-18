6dc6b523ab8269456d2d7146ce642287fd5cb057
Matheus-Nasci <nasci.olv@outlook.com>
2026-05-17 23:03:23 -0300

    fix: health check e swagger

---
+++ Controller/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nebulosa.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult HealthcheckApplication()
+    {
+        return Ok("Health");
+    }
+}
